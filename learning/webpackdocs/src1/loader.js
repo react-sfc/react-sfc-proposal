f26c37ff32bca8e23c2ee4a69ade6b6eebdc24c0
sw-yx <swyx@dontemail.me>
2019-03-17 00:26:18 -0700

    get rid of unused code

--- learning/webpackdocs/src1/loader.js
+++
@@ -1,9 +0,0 @@
-import { getOptions } from 'loader-utils'
-
-export default function loader(source) {
-  const options = getOptions(this)
-
-  source = source.replace(/\[name\]/g, options.name)
-
-  return `export default ${JSON.stringify(source)}`
-}
